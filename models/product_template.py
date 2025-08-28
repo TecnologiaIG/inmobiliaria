7b6c58c6363f228b8c3bacf0264b7ce18f095d0d
Rodrigo Fernández <rodrigo@aquih.com>
2025-08-28 18:13:42 +0000

    Quitar imports innecesarios

--- models/product_template.py
+++ models/product_template.py
@@ -4,11 +4,7 @@
 import itertools
 import psycopg2
 
-from odoo.addons import decimal_precision as dp
-
 from odoo import api, fields, models, tools, _
-from odoo.exceptions import ValidationError, RedirectWarning, except_orm
-from odoo.tools import pycompat
 
 
 class ProductTemplate(models.Model):
