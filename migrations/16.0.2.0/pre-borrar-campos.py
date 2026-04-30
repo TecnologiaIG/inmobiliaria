73ceced4c96fa8b6d581346d28e9641dbdda853a
Allan Rosales <helpdesk@intensegroup.com.gt>
2026-04-30 10:58:42 -0600

    v19 prep: remove obsolete v16 pre-migration script
    migrations/16.0.2.0/pre-borrar-campos.py used 'from odoo.upgrade import util'
    which doesn't exist in v19. The field crm.lead.bodega_id removal was
    already done in the v16 → v19 upgrade automation. Removing the script
    unblocks button_immediate_upgrade(inmobiliaria) on the upgraded DB.

--- migrations/16.0.2.0/pre-borrar-campos.py
+++
@@ -1,9 +0,0 @@
-import logging
-from odoo.upgrade import util
-
-_logger = logging.getLogger(__name__)
-
-
-def migrate(cr, version):
-    util.remove_field(cr, 'crm.lead', 'bodega_id')
-    _logger.info("Campos borrados")
